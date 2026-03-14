14f9b68e205c14622abd95eab2a1a268d35d843c
Davi Gabriel Domingues <domingues.gabriel.davi@gmail.com>
2026-03-13 22:34:29 -0300

    Refactor comments and formatting in helloWorld.asm

--- programas_assembly_riscV/helloWorld.asm
+++ programas_assembly_riscV/helloWorld.asm
@@ -7,12 +7,12 @@
 
 main:
     # 1. Preparar os argumentos para a chamada de sistema
-    li a7, 4            # O código 4 no registrador a7 diz ao sistema: "Vou imprimir uma string"
-    la a0, mensagem     # O registrador a0 deve conter o endereço da string na memória
+    li a7, 4 # O código 4 no registrador a7 diz ao sistema: "Vou imprimir uma string"
+    la a0, mensagem # O registrador a0 deve conter o endereço da string na memória
     
     # 2. Executar a chamada
-    ecall               # "Ei, sistema operacional! Olha o a7 e faz o que eu pedi."
+    ecall # "Ei, sistema operacional! Olha o a7 e faz o que eu pedi."
 
     # 3. Finalizar o programa (Boa prática para não travar o simulador)
-    li a7, 10           # O código 10 encerra o programa com sucesso
+    li a7, 10 # O código 10 encerra o programa com sucesso
     ecall
